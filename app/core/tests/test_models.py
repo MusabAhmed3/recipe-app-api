90e1ff2c0f618af729b49451dcb1e211c1e64b92
MusabAhmed3 <amusab072@gmail.com>
2024-12-10 17:43:05 +0500

    Created tests for user model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -14,3 +14,33 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        sample_emails = [
+            ['test@EXAMPLE.COM', 'test@example.com'],
+            ['Test@Example.com', 'Test@example.com'],
+            ['TEST@example.COM', 'TEST@example.com'],
+            ['tesT@EXAMPLE.com', 'tesT@example.com']
+        ]
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(
+                email,
+                'testpass123'
+            )
+            self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(
+                '',
+                'testpass123'
+            )
+
+    def test_create_superuser(self):
+        user = get_user_model().objects.create_superuser(
+            'test@example.com',
+            'testpass123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
